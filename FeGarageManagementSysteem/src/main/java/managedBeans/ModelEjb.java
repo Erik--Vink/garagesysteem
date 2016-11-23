36d8045373718d866255cbb7e62c78d6c05d26ab
dreaneii <kdominicus@gmail.com>
2016-11-23 14:56:33 +0100

    Geprobeerd View voor het model van een auto toe te voegen Helaas wil hij de dropdown niet laden

---
+++ FeGarageManagementSysteem/src/main/java/managedBeans/ModelEjb.java
@@ -0,0 +1,57 @@
+package managedBeans;
+
+import domain.Brand;
+import domain.Model;
+import repositories.BrandRepository;
+import repositories.ModelRepository;
+
+import javax.ejb.EJB;
+import javax.ejb.Stateful;
+import javax.ejb.Stateless;
+import javax.inject.Named;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Kenzo Dominicus on 23-11-2016.
+ */
+@Stateless
+@Named("ModelEjb")
+public class ModelEjb {
+    private Model model;
+
+    @EJB
+    ModelRepository modelRepository;
+    @EJB
+    BrandRepository brandRepository;
+
+    public ModelEjb(){
+        this.model = new Model();
+    }
+
+    public Model getModel(){
+        return model;
+    }
+
+    public List<Brand> getBrands(){
+        List<Brand> brands = null;
+        try {
+            brands = brandRepository.getAll();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return brands;
+    }
+
+    public String save(){
+        try {
+            modelRepository.save(model);
+            model = new Model();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "/index?faces-redirect=true";
+    }
+
+
+}
